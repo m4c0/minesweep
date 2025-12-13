73537addc379c5949b6f8a6c1d5eb125dfaffd12
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-13 21:02:08 +0000

    wasm stub

--- v_wasm.cpp
+++ v_wasm.cpp
@@ -1,2 +1,19 @@
 module v;
+import vinyl;
+
+struct app_stuff;
+struct sized_stuff;
+vinyl::v<app_stuff, sized_stuff> g;
+
+struct app_stuff {
+};
+struct sized_stuff {
+};
+
+const int i = [] {
+  g.on_frame() = [] {};
+  g.setup();
+
+  return 0;
+}();
 
